d0bacc4fd3e6e385032e631d5fa95007957abfdd
vagrant <vagrant@vagrant.vm>
2021-10-16 03:40:47 +0000

    embedding class now takes updated message struct

--- intents_consolidator/src/embedding.cpp
+++ intents_consolidator/src/embedding.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <Eigen/Dense>
+#include <cassert>
 
 using Eigen::Matrix;
 
 struct embedding_msg {
-    int height; // Total number of rows
-	int  row_step; // Number of floats in a row
-	double* data;  // Actual point data, size is (row_step*height)
+	std::vector<double> data;  // Actual point data, size is (row_step*height)
 	std::string comparison_type; // Either cosine or distance
 	std::string type; // Unique type of the embedding
 };
@@ -15,38 +14,42 @@ struct embedding_msg {
 class Embedding
 {
     public:
-        Embedding(struct embedding_msg *msg) : m(msg->data, msg->height, msg->row_step) {
-            std::cout << m << std::endl;
-            comparison_type = msg->comparison_type;
-            type = msg->type;
-            // m(msg->data, msg->height, msg->row_step);
-            // m.resize(msg->height, msg->row_step);
+        Embedding(struct embedding_msg &msg) : vector{msg.data.data(), msg.data.size()} {
+            //std::cout << vector << std::endl;
+            comparison_type = msg.comparison_type;
+            type = msg.type;
         }
         
-        double compare(Embedding *other) {
-            // Return Fro
+        double compare(const Embedding &other) {
+            assert(other.vector.size() != vector.size());
+            std::cout << "The two matrices are not the same size\n";
+            // Return Frobenius norm of the difference between this matrix and other
             if (comparison_type == "distance") {
-                Eigen::MatrixXd temp = m - other->m;
+                Eigen::VectorXd temp = vector - other.vector;
                 // std::cout << temp << std::endl;
                 return temp.norm();
             }
-            if (comparison_type == "cosine") {return 0.0;}
+            if (comparison_type == "cosine") {
+                return 0.0;
+            }
             return 0.0;
         }
+        Eigen::Map<Eigen::VectorXd> vector;
     private:
-        Eigen::Map<Eigen::MatrixXd> m;
+        
         std::string comparison_type;
         std::string type;
 };
 
 int main()
 {
-    double a[12] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
-    struct embedding_msg msg = {2, 6, a, "distance", "test"};
-    double b[12] = {7.0, 8.0, 9.0, 10.0, 5.0, 6.0};
-    struct embedding_msg msg2 = {2, 6, b, "distance", "test"};
-    Embedding test(&msg);
-    Embedding test2(&msg2);
-    std::cout << test2.compare(&test)<< std::endl;
+    std::vector<double> a = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    struct embedding_msg msg = {a, "distance", "test"};
+    std::vector<double> b = {7.0, 8.0, 9.0, 10.0, 5.0, 6.0};
+    struct embedding_msg msg2 = {b, "distance", "test"};
+    Embedding test(msg);
+    a[4] = 100000.0;
+    std::cout << test.vector << std::endl;
+    Embedding test2(msg2);
+    std::cout << test2.compare(test)<< std::endl;
 }
-// Matrix<typename Scalar, int RowsAtCompileTime, int ColsAtCompileTime>
