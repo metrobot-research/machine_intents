dee3d449b702aba9b44430fc9ae4b620c25b43c5
Jaiveer Singh <j.singh@berkeley.edu>
2021-10-16 02:45:15 +0000

    Update license and package info

--- face_detection/setup.py
+++ face_detection/setup.py
@@ -4,7 +4,7 @@ package_name = 'face_detection'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='manav',
-    maintainer_email='manav@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Jaiveer Singh',
+    maintainer_email='j.singh@berkeley.edu',
+    description='Facial detection wrapper for the MetroBot Intents system',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
